3f7cdc597a1bf0fcbbd35db943190060c9a32b4d
Edward <edward.neto@triersistemas.com.br>
2019-05-13 16:17:52 -0300

    Conexao com o banco de dados

--- src/main/java/br/com/edward/restfull/domain/Produto.java
+++ src/main/java/br/com/edward/restfull/domain/Produto.java
@@ -1,22 +1,39 @@
 package br.com.edward.restfull.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
 import br.com.edward.restfull.model.ProdutoModel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
 @Getter
-public class Produto {
-   
-    private static Long cont = 0L;
-    
-    private final Long id;
-    private String nome;
-    private Double preco;
-    private Integer qtd;
 
-    public Produto() {
-        this.id = ++cont;
-        this.qtd = 0;
-    }
+@Entity
+@Table(name = "produto")
+public class Produto {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @NotNull
+    @Column(name="nome", length = 128)
+    private String nome;
+    
+    @NotNull
+    @Column(name="preco")
+    private Double preco;
+    
+    @NotNull
+    @Column(name="qtd")
+    private Integer qtd;
     
     public Produto(ProdutoModel model) {
         this();
--- src/main/java/br/com/edward/restfull/service/impl/ProdutoServiceImpl.java
+++ src/main/java/br/com/edward/restfull/service/impl/ProdutoServiceImpl.java
@@ -1,48 +1,45 @@
 package br.com.edward.restfull.service.impl;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.edward.restfull.domain.Produto;
 import br.com.edward.restfull.model.ProdutoModel;
+import br.com.edward.restfull.repository.ProdutoRepository;
 import br.com.edward.restfull.service.ProdutoService;
 
+@Transactional
 @Service
 public class ProdutoServiceImpl implements ProdutoService {
 
-    private static List<Produto> lista = new ArrayList<>();
-
+    @Autowired
+    private ProdutoRepository produtoRepository;
+    
     @Override
     public Produto consultar(Long idProduto) {
-        return lista.stream().filter(item -> idProduto.equals(item.getId())).findAny().orElse(null);
+        return produtoRepository.findById(idProduto).orElse(null);
     }
 
     @Override
     public Produto cadastrar(ProdutoModel model) {
         
-        Produto produto = this.consultar(model.getId());
-        if (Objects.isNull(produto)) {
-            
-            produto = new Produto(model);
-            lista.add(produto);
-            return produto;
-        }
-        throw new RuntimeException("Produto já existe");
+        return produtoRepository.save(new Produto(model));
     }
 
     @Override
     public List<Produto> mostrarTudo() {
-        return lista;
+        return produtoRepository.findAll();
     }
 
     @Override
     public Produto remover(Long id) {
         Produto produto = this.consultar(id);
         if (Objects.nonNull(produto)) {
-            lista.remove(produto);
+            produtoRepository.delete(produto);
         }
         return produto;
     }
@@ -52,6 +49,7 @@ public class ProdutoServiceImpl implements ProdutoService {
         Produto produto = this.consultar(model.getId());
         if (Objects.nonNull(produto)) {
             produto.alterar(model);
+            return produtoRepository.save(produto);
         }
         return produto;
     }
